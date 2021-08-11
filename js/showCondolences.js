54ecb039c456ba0f8b5ddcd96fa0dc574a05f548
Rotimi Adebiyi <r.s.adebiyi@gmail.com>
2021-08-11 07:34:44 +0100

    Update showCondolences.js

--- js/showCondolences.js
+++ js/showCondolences.js
@@ -3,6 +3,7 @@ import { showCondolences } from "./datamanagement.js";
 
 const showMessage = () => {
   const showCard = document.getElementById('showCondolences');
+  console.log(showCondolences())
   showCard.addEventListener('load', showCondolences());
 }
 showMessage();
